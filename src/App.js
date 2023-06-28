c9c3f2a0a6b3b10f060b4b78e249d3ab5ec5d86e
Dharani Vora <dharanivora@outlook.com>
2023-06-28 18:51:19 -0400

    feat: implement game board

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./Styles.css";
+
+function Square({ value }) {
+  return <button className="square">{value}</button>;
+}
+
+function Board() {
+  return (
+    <div className="board">
+      <Square value="1" />
+      <Square value="2" />
+      <Square value="3" />
+      <Square value="4" />
+      <Square value="5" />
+      <Square value="6" />
+      <Square value="7" />
+      <Square value="8" />
+      <Square value="9" />
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to Tic-Tac-Toe!</h1>
+      <Board />
     </div>
   );
 }
