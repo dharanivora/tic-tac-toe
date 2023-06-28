642e2f679b7e9ccd7c4c1cd55e6dadd93f9441fa
Dharani Vora <dharanivora@outlook.com>
2023-06-28 19:14:18 -0400

    feat: build button click interactivity

--- src/App.js
+++ src/App.js
@@ -1,32 +1,43 @@
 import "./Styles.css";
+import { useState } from "react";
 
-function Square({ value }) {
-  return <button className="square">{value}</button>;
+function Square() {
+    const [value, setValue] = useState(null);
+
+    function handleClick() {
+        setValue("X");
+    }
+
+    return (
+        <button className="square" onClick={handleClick}>
+            {value}
+        </button>
+    );
 }
 
 function Board() {
-  return (
-    <div className="board">
-      <Square value="1" />
-      <Square value="2" />
-      <Square value="3" />
-      <Square value="4" />
-      <Square value="5" />
-      <Square value="6" />
-      <Square value="7" />
-      <Square value="8" />
-      <Square value="9" />
-    </div>
-  );
+    return (
+        <div className="board">
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+            <Square />
+        </div>
+    );
 }
 
 function App() {
-  return (
-    <div>
-      <h1>Welcome to Tic-Tac-Toe!</h1>
-      <Board />
-    </div>
-  );
+    return (
+        <div>
+            <h1>Welcome to Tic-Tac-Toe!</h1>
+            <Board />
+        </div>
+    );
 }
 
 export default App;
