1bcf69fc29a4d90fe5841613de98a0febfe72a01
Dharani Vora <dharanivora@outlook.com>
2023-06-29 18:05:03 -0400

    chore: wip-implement display of value when each button is clicked

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
-import "./Styles.css";
 import { useState } from "react";
+import "./Styles.css";
 
-function Square() {
-    const [value, setValue] = useState(null);
-
-    function handleClick() {
-        setValue("X");
-    }
-
+function Square({ value, callWhenClicked }) {
     return (
-        <button className="square" onClick={handleClick}>
+        <button className="square" onClick={callWhenClicked}>
             {value}
         </button>
     );
 }
 
 function Board() {
+    const [squares, setSquares] = useState(Array(9).fill(null));
+
+    function handleClick(index) {
+        const updatedSquares = squares.slice();
+        updatedSquares[index] = "X";
+        setSquares(updatedSquares);
+    }
+
     return (
         <div className="board">
-            <Square />
-            <Square />
-            <Square />
-            <Square />
-            <Square />
-            <Square />
-            <Square />
-            <Square />
-            <Square />
+            <Square value={squares[0]} callWhenClicked={() => handleClick(0)} />
+            <Square value={squares[1]} callWhenClicked={() => handleClick(1)} />
+            <Square value={squares[2]} callWhenClicked={() => handleClick(2)} />
+            <Square value={squares[3]} callWhenClicked={() => handleClick(3)} />
+            <Square value={squares[4]} callWhenClicked={() => handleClick(4)} />
+            <Square value={squares[5]} callWhenClicked={() => handleClick(5)} />
+            <Square value={squares[6]} callWhenClicked={() => handleClick(6)} />
+            <Square value={squares[7]} callWhenClicked={() => handleClick(7)} />
+            <Square value={squares[8]} callWhenClicked={() => handleClick(8)} />
         </div>
     );
 }
